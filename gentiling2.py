incorrect computation of indices

gentiling2.py
@@ -22,9 +22,15 @@ def mktransform(x, y, theta, scale):
         [0, 0, 1],
     ])
 
+def index_opp(i):
+    return [3, 4, 1, 2][i - 1]
+def index_nei(i):
+    return [2, 3, 2, 3][i - 1]
+
 @dataclass
 class Tile:
     thick: bool
+    index: int
     transform: np.matrix
 
     def get_point(self, lx: float, ly: float) -> tuple[float, float]:
@@ -50,27 +56,57 @@ class Tile:
     def subdivide(self) -> list["Tile"]:
         s = sin(54 * pi / 180)
         subscale = 1 / (2 * s)
+        ind = self.index
+        opp = index_opp(self.index)
+        nei = index_nei(self.index)
 
         if self.thick:
             c = cos(54 * pi / 180)
             midx, midy = 0, 2 * s - 1 / (2 * s)
-            T1 = Tile(True, self.transform * mktransform(midx, midy, 180, subscale))
-            T2 = Tile(True, self.transform * mktransform(0, 2*s, 180-36, subscale))
-            T3 = Tile(True, self.transform * mktransform(0, 2*s, 180+36, subscale))
-            t1 = Tile(False, self.transform * mktransform(-c, s, 90+36, subscale))
-            t2 = Tile(False, self.transform * mktransform(c, s, 90-36, subscale))
+            T1 = Tile(True, opp, self.transform * mktransform(midx, midy, 180, subscale))
+            T2 = Tile(True, opp, self.transform * mktransform(0, 2*s, 180-36, subscale))
+            T3 = Tile(True, opp, self.transform * mktransform(0, 2*s, 180+36, subscale))
+            t1 = Tile(False, nei, self.transform * mktransform(-c, s, 90+36, subscale))
+            t2 = Tile(False, nei, self.transform * mktransform(c, s, 90-36, subscale))
             return [T1, T2, T3, t1, t2]
         else:
             c, s = cos(72 * pi / 180), sin(72 * pi / 180)
-            T1 = Tile(True, self.transform * mktransform(0, 0, 18, subscale))
-            T2 = Tile(True, self.transform * mktransform(0, 0, 180-18, subscale))
-            t1 = Tile(False, self.transform * mktransform(-2*c, 0, 270-18, subscale))
-            t2 = Tile(False, self.transform * mktransform(-2*c, 0, 90+18, subscale))
+            T1 = Tile(True, ind, self.transform * mktransform(0, 0, 18, subscale))
+            T2 = Tile(True, ind, self.transform * mktransform(0, 0, 180-18, subscale))
+            t1 = Tile(False, opp, self.transform * mktransform(-2*c, 0, 270-18, subscale))
+            t2 = Tile(False, opp, self.transform * mktransform(-2*c, 0, 90+18, subscale))
             return [T1, T2, t1, t2]
 
-def set_color(ctx: cairo.Context, hex: int):
+def subdivide_with_indices(t: Tile, n: int, origin_index: int) -> list[Tile]:
+    if n == 0:
+        t.index = origin_index
+        return [t]
+
+    # We have a forced index for all subdivisions
+    next_forced_index = origin_index
+
+    for sub in t.subdivide(): # lazy
+        subdivide_with_indices(sub, n - 1, next_forced_index)
+
+    ...
+
+    t.subdivide_others()
+    # propagate index
+
+    return [sub1, ...]
+
+def mkcolor(hex: int):
     r, g, b = (hex >> 16) & 0xff, (hex >> 8) & 0xff, (hex & 0xff)
-    ctx.set_source_rgb(r / 255, g / 255, b / 255)
+    return r / 255, g / 255, b / 255
+
+def mkcolor_darkened(hex: int, index: int):
+    r, g, b = mkcolor(hex)
+    darkening_amount = [0, 0.25, 0.5, 0.75][index - 1]
+    da = darkening_amount
+    return r * da, g * da, b * da
+
+def set_color(ctx: cairo.Context, hex: int):
+    ctx.set_source_rgb(*mkcolor(hex))
 
 def draw_tile(ctx: cairo.Context, t: Tile, color: int | None = None):
     ps = t.get_points()
@@ -82,9 +118,19 @@ def draw_tile(ctx: cairo.Context, t: Tile, color: int | None = None):
     if color is not None:
         set_color(ctx, color)
     elif t.thick:
-        set_color(ctx, 0x62ae19)
+        start, _, end, _ = t.get_points()
+        gradient = cairo.LinearGradient(*start, *end)
+        gradient.add_color_stop_rgb(0, *mkcolor_darkened(0x62ae19, t.index))
+        gradient.add_color_stop_rgb(1, *mkcolor_darkened(0x62ae19, index_opp(t.index)))
+        ctx.set_source(gradient)
     else:
-        set_color(ctx, 0x80afe1)
+        color = 0x80afe1
+        # color = 0x62ae19
+        start, _, end, _ = t.get_points()
+        gradient = cairo.LinearGradient(*start, *end)
+        gradient.add_color_stop_rgb(0, *mkcolor_darkened(color, t.index))
+        gradient.add_color_stop_rgb(1, *mkcolor_darkened(color, index_opp(t.index)))
+        ctx.set_source(gradient)
 
     ctx.fill_preserve()
 
@@ -107,14 +153,29 @@ def main():
     ctx.translate(w / 2, h / 2)
     ctx.scale(1, -1)
 
-    tiles = [Tile(True, mktransform(0, -400, 0, 430))]
+    tiles = [Tile(True, 1, mktransform(0, -400, 0, 430))]
     steps = 5
 
     for i in range(steps):
         tiles = subdivide_set(tiles)
 
+    # Easiest path forward:
+    # -> Construct adjacency graph by looking at point coordinates (approximately)
+    # -> Solve for indices in a graph traversal manner
+
     for tile in tiles:
         draw_tile(ctx, tile)
+
+    # gradient = cairo.LinearGradient(-100, 0, 100, 0)
+    # gradient.add_color_stop_rgb(0, *mkcolor(0xff0000))
+    # gradient.add_color_stop_rgb(1, *mkcolor(0x0000ff))
+    # ctx.set_source(gradient)
+    # ctx.move_to(-w / 2, -h / 2)
+    # ctx.line_to(w / 2, -h / 2)
+    # ctx.line_to(w / 2, h / 2)
+    # ctx.line_to(-w / 2, h / 2)
+    # ctx.close_path()
+    # ctx.fill()
 
     surface.write_to_png("tiling.png")
 